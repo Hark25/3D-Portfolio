7a7d59be9b99e32fd9993871b8629a71e80b1105
Hark25 <harklit25@gmail.com>
2025-07-14 15:12:00 -0700

    Hero Section done, added cards
    Cards added that display important stats like years of experience, clients, and projects made.

--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -6,7 +6,7 @@ const HeroLights = () => {
             <spotLight 
             position={[2,5,6]} 
             angle={0.15}
-            intensity={100}
+            intensity={90}
             penumbra={0.2}
             color={"white"}
             />
@@ -14,7 +14,7 @@ const HeroLights = () => {
             <spotLight 
             position={[4,5,4]} 
             angle={0.3}
-            intensity={40}
+            intensity={30}
             penumbra={0.5}
             color={"#4cc9f0"}
             />
@@ -22,27 +22,27 @@ const HeroLights = () => {
             <spotLight 
             position={[-3,5,5]} 
             angle={0.4}
-            intensity={60}
+            intensity={50}
             penumbra={1}
             color={"#9d4edd"}
             />
 
             <primitive 
-                object={new THREE.RectAreaLight('#A259ff', 8, 3, 2)}
+                object={new THREE.RectAreaLight('#A259ff', 5, 3, 2)}
                 position={[1,3,4]}
-                intensity={15}
+                intensity={2}
                 rotation={[-Math.PI/4, Math.PI/4, 0]}
             />
 
             <pointLight 
                 position={[0,1,0]}
-                intensity={10}
+                intensity={4}
                 color={"#7209b7"}
             />
 
             <pointLight 
                 position={[1,2,-2]}
-                intensity={10}
+                intensity={4}
                 color={"#0d00a4"}
             />
         </>
