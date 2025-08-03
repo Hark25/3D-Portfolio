2ed3096e5015bec876d318a2e7e49f18de6c1ace
Hark25 <harklit25@gmail.com>
2025-08-03 13:28:43 -0700

    Footer complete

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,28 @@
+import { socialImgs } from '../Constants/index.js';
+
 const Footer = () => {
     return ( 
-        <div>
-            
-        </div>
+        <footer className="footer">
+            <div className="footer-container">
+
+                <div className="flex flex-col justify-center md:items-start items-center">
+                    <a href="/"> Thank You </a>
+                </div>
+
+                <div className="socials">
+                    {socialImgs.map((img) => (
+                        <a href={img.link} target='_blank' rel='noopener noreferrer' className='icon' key={img.link}>
+                            <img src={img.imgPath} alt={img.name} />
+                        </a>
+                    ))}
+                </div>
+
+                <div className="flex flex-col justify-center">
+                    <p className='text-center md:text-end'>© {new Date().getFullYear()} Harkeerat Lit. All rights reservered.</p>
+                </div>
+
+            </div>
+        </footer>
      );
 }
  
